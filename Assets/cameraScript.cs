80572b9c53bd24442204e98f40b56c2da7a8d24a
juanfel <jf3dsaber@gmail.com>
2014-05-26 19:56:34 -0400

    Commit inicial

---
+++ Assets/cameraScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraScript : MonoBehaviour {
+	Vector3 cameraDir;
+	float speed = 0.01f;
+	// Use this for initialization
+	void Start () 
+	{
+		cameraDir = new Vector3 (1, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentPos = transform.position;
+		transform.position = cameraDir * speed + currentPos;
+	}
+}
---
+++ Assets/movementScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movementScript : MonoBehaviour {
+	Vector3 movementVector;
+	float speed = 0.4f;
+	// Use this for initialization
+	void Start () {
+		movementVector = new Vector3 ();
+		movementVector.z = 0;
+	}
+	
+	// Update is called once per frame
+	// Changes direction of movement depending of what button is pressed
+	// Also updates the camera
+	void Update () {
+
+		movementVector.y = Input.GetAxis ("Vertical");
+		movementVector.x = Input.GetAxis ("Horizontal");
+		Vector3 currentPos = transform.position;
+		transform.position = movementVector * speed + currentPos;
+		movementVector = new Vector3 (0, 0, 0);
+	}
+	void FixedUpdate(){
+
+		}
+}
