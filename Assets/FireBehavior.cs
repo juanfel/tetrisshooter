4f41c2ee3b0ff269d2a3d8fc0faeeb531dad84fb
juanfel <jf3dsaber@gmail.com>
2014-06-16 20:03:41 -0400

    Cambios a creacion de bloques

---
+++ Assets/FireBehavior.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBehavior : MonoBehaviour {
+	public GameObject ShapeManager;
+	private ShapeBehavior shapebehavior; 
+	private int alto = 2;
+	private int ancho = 4;
+	bool[][] figure;
+	// Use this for initialization
+	void Start () {
+		shapebehavior = (ShapeBehavior)ShapeManager.GetComponent(typeof(ShapeBehavior));
+		shapebehavior.alto = alto;
+		shapebehavior.ancho = ancho;
+		shapebehavior.figure = createDebugFigure (alto,ancho); 
+		shapebehavior.bloqueName = "SmallBlockSprite";
+
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1") == true) 
+		{
+			shapebehavior.x0 = transform.position.x;
+			shapebehavior.y0 = transform.position.y;
+			shapebehavior.StartShape();
+		}
+	}
+
+	//Crea una figura para propositos de testing y la asigna al shapebehavior
+	bool[][] createDebugFigure(int alto, int ancho)
+	{
+		bool[][] figure = new bool[alto][];
+		for (int i = 0; i<alto; i++)
+		{
+			figure[i] = new bool[ancho];
+		}
+		figure[0]=new bool[]{true,true,false,true};
+		figure[1]=new bool[]{true,true,true,true};
+		return figure;
+
+	}
+}
---
+++ Assets/ShapeBehavior.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+//Esta clase instancia una figura en el espacio y hace que cada bloque avance ocupando
+//su patron de movimiento
+public class ShapeBehavior : MonoBehaviour {
+	public string bloqueName; //Nombre del bloque a instanciar
+	public GameObject bloque;	//Aqui va el tipo de bloque que queremos usar para el shape 
+	public bool[][] figure;   //Aqui va la figura, que es instanciada en otra parte
+								//1-> hay bloque en esa posicion. 0-> no hay bloque
+	public int ancho;
+	public int alto;
+
+	public float x0;
+	public float y0;
+
+	// Reemplaza los valores donde deben ir los bloques por los bloques mismos
+	// Ademas los instancia en el mundo. Parte desde el extremo (0,0). O sea, va rellenando
+	// al reve
+	public void StartShape () {
+		float x = x0;
+		float y = y0;
+		bloque = (GameObject)Resources.Load (bloqueName);
+		/*for (int i = 0; i < alto; i++) {
+			for (int j = 0; j < ancho; j++) {
+				if(figure[i][j] == true)
+				{
+					Instantiate(bloque,new Vector3(x,y,0),Quaternion.identity);
+				}
+
+				y -= 1;
+			}
+			y = y0;
+
+			x += 1;
+		}*/
+		foreach (bool[] i in figure) {
+			foreach(bool j in i)
+			{
+				if(j == true)
+				{
+					Instantiate(bloque,new Vector3(x,y,0),Quaternion.identity);
+				}
+				//y -= bloque.collider2D.bounds.size.y;
+				y -=1;
+			}
+			y = y0;
+			//x += bloque.collider2D.bounds.size.x;
+			x += 1;
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
